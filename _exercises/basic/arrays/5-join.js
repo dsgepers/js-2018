8533d0db0c5f840d26469996ca4ae5a44b5962dc
Dennis Schepers <dennis.schepers@jstack.eu>
2018-03-20 13:02:31 +0100

    basic/array/5 join

--- _exercises/basic/arrays/5-join.js
+++ _exercises/basic/arrays/5-join.js
@@ -12,6 +12,7 @@ const array = [1, 2, 3, 4, 5];
     1. display the list comma separated (to console.log)
 
  */
+console.log(array.join(", "));
 
 /*
 
@@ -19,4 +20,8 @@ const array = [1, 2, 3, 4, 5];
     <ul><li>1</li><li>2</li>...</ul>
 
  */
+const htmlElements = array.map((item) => "\t<li>" + item + "</li>");
+htmlElements.unshift("<ul>");
+htmlElements.push("</ul>");
+console.log(htmlElements.join("\r\n"));
 
