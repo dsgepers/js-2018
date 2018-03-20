433442ee74a474d746be10bd01892861ac595a21
Dennis Schepers <dennis.schepers@jstack.eu>
2018-03-20 12:44:59 +0100

    basic/arrays/3 map

--- _exercises/basic/arrays/3-map.js
+++ _exercises/basic/arrays/3-map.js
@@ -12,5 +12,7 @@ const list = [2,4,6];
     1. Use the map method to square all even numbers from the array
 
  */
+const result = list.map((item) => Math.pow(2, item));
+console.log(result);
 
 // const squared = list.map ...
