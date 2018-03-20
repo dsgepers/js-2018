8e75d3b222df0fa1b182276a435682219273237a
Dennis Schepers <dennis.schepers@jstack.eu>
2018-03-20 12:32:06 +0100

    basic/arrays/1 arrays

--- _exercises/basic/arrays/1-for-loop.js
+++ _exercises/basic/arrays/1-for-loop.js
@@ -12,7 +12,7 @@ const list = [1,2,3,4,5,6];
     1. Use a for loop to log every item in the array
 
  */
-
+list.forEach((item) => console.log(item));
 /*
 
     2. Use a for loop to filter all even numbers from the array
@@ -21,6 +21,8 @@ const list = [1,2,3,4,5,6];
 
 const even = [];
 
+list.forEach((item) => { if (item % 2 == 0 ){ even.push(item)} });
+
 /*
 
     3. Use a for loop to square every item in the even list.
@@ -28,3 +30,6 @@ const even = [];
  */
 
 const squared = [];
+list.forEach((item) => { squared.push(Math.pow(2, item)) });
+
+console.log(even, squared);
